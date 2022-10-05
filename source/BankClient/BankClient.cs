077163217ccf5f2f72331e020e6293911584db98
Tomás Maria De Aquino da Silva de Araújo Pereira <ist195682@tecnico.ulisboa.pt>
2022-10-05 23:53:57 +0100

    Initial client base

--- source/BankClient/BankClient.cs
+++ source/BankClient/BankClient.cs
@@ -1,4 +1,4 @@
-﻿using DADPerfectChannelClient;
+﻿/* using DADBankClientClient; */
 using Grpc.Core;
 using Grpc.Core.Interceptors;
 using Grpc.Net.Client;
@@ -7,12 +7,54 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 internal class BankClient
 {
-    private static void Main(string[] args)
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        const int ServerPort = 1001;
+        const string ServerHostname = "localhost";
+        AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+        var clientInterceptor = new ClientInterceptor();
+        GrpcChannel channel = GrpcChannel.ForAddress("http://" + ServerHostname + ":" + ServerPort);
+        CallInvoker interceptingInvoker = channel.Intercept(clientInterceptor);
+        var client = new BankClientService.BankClientServiceClient(interceptingInvoker);
+        BankClientRequest registerRequest = new BankClientRequest { Message = "test" };
+        BankClientReply reply = client.Test(registerRequest);
+        Console.WriteLine("reply: " + reply.Status);
+        Console.ReadKey();
+    }
+
+    public class ClientInterceptor : Interceptor
+    {
+        // private readonly ILogger logger;
+
+        //public GlobalServerLoggerInterceptor(ILogger logger) {
+        //    this.logger = logger;
+        //}
+
+        public override TResponse BlockingUnaryCall<TRequest, TResponse>(TRequest request, ClientInterceptorContext<TRequest, TResponse> context, BlockingUnaryCallContinuation<TRequest, TResponse> continuation)
+        {
+
+            Metadata metadata = context.Options.Headers; // read original headers
+            if (metadata == null) { metadata = new Metadata(); }
+            metadata.Add("dad", "dad-value"); // add the additional metadata
+
+            // create new context because original context is readonly
+            ClientInterceptorContext<TRequest, TResponse> modifiedContext =
+                new ClientInterceptorContext<TRequest, TResponse>(context.Method, context.Host,
+                    new CallOptions(metadata, context.Options.Deadline,
+                        context.Options.CancellationToken, context.Options.WriteOptions,
+                        context.Options.PropagationToken, context.Options.Credentials));
+            Console.Write("calling server...");
+            TResponse response = base.BlockingUnaryCall(request, modifiedContext, continuation);
+            return response;
+        }
+
     }
 }
