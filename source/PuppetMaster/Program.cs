ad204bb1e9cbfd62ef853baa5152037ece553a36
Tomás Pereira <tomas.maria.pereira@hotmail.com>
2022-10-08 16:30:13 +0100

    Failed puppet master implementation lmao

--- source/PuppetMaster/Program.cs
+++ source/PuppetMaster/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,13 +14,23 @@ internal class PuppetMaster
     static void Main(string[] args)
     {
         //Find Common file directory
-        var currentPath = Directory.GetCurrentDirectory();
-        var parentPath = Path.GetFullPath(Path.Combine(currentPath, @"..\"));
-        var commonPath = Path.GetFullPath(Path.Combine(parentPath, "\\configuration_sample.txt"));
+        //var currentPath = Directory.GetCurrentDirectory();
+        //var parentPath = Path.GetFullPath(Path.Combine(currentPath, @"..\"));
+        //var commonPath = Path.GetFullPath(Path.Combine(parentPath, "\\configuration_sample.txt"));
 
-        string[] lines = File.ReadAllLines(commonPath);
-        foreach (string line in lines)
-            Console.WriteLine(line);
+        string[] lines = File.ReadAllLines(@"C:\Users\tomas\source\repos\DAD Project\source\Common\configuration_sample.txt");
+        foreach (string command in lines) {
+            string[] tokens = command.Split(' ');
 
+            if (tokens[0].Equals("P")) { //New process
+
+                switch (tokens[2])
+                {
+                    case "client":
+                        Process.Start(@"C:\Users\tomas\source\repos\DAD Project\source\Customer\Customer.cs", tokens[1]);
+                        break;
+                }
+            } 
+        }
     }
 }
