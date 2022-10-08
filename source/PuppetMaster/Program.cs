3da84450def03cc38f02b6b580e364d8083c53f6
Tomás Pereira <tomas.maria.pereira@hotmail.com>
2022-10-08 01:22:46 +0100

    Misc. skeleton stuff

---
+++ source/PuppetMaster/Program.cs
@@ -0,0 +1,25 @@
+﻿using Common;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+//Simple text file parser to kickstart process initialization
+internal class PuppetMaster
+{
+    static void Main(string[] args)
+    {
+        //Find Common file directory
+        var currentPath = Directory.GetCurrentDirectory();
+        var parentPath = Path.GetFullPath(Path.Combine(currentPath, @"..\"));
+        var commonPath = Path.GetFullPath(Path.Combine(parentPath, "\\configuration_sample.txt"));
+
+        string[] lines = File.ReadAllLines(commonPath);
+        foreach (string line in lines)
+            Console.WriteLine(line);
+
+    }
+}
