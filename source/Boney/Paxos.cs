90a56d18f65993b36e957056c4494664b9a76eb8
diogoDSN <diogofgsnogueira@gmail.com>
2022-10-10 23:11:46 +0100

    Paxos proto and initial plan; Consensus -> Boney

---
+++ source/Boney/Paxos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boney
+{
+    internal class Paxos
+    {
+
+
+        private int proposed_value = -1;
+        private ConcurrentDictionary<int, int> commited_values = new ConcurrentDictionary<int, int>();
+        private ConcurrentDictionary<int, int> learned_values = new ConcurrentDictionary<int, int>();
+
+        public Paxos()
+        {
+
+        }
+
+        public int Consensus(int consensus_number, int proposed_value)
+        {
+
+        }
+
+    }
+}
