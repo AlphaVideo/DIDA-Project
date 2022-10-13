96537f66a05408a0ef9ff57556a1a1b968843949
ShadowGoblet20354 <62452709+ShadowGoblet20354@users.noreply.github.com>
2022-10-13 12:24:21 +0100

    Started implementing BoneyService. Only missing frozen logic.

--- source/Boney/BoneyServiceImpl.cs
+++ source/Boney/BoneyServiceImpl.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Grpc.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,21 @@ namespace Boney
 {
     internal class BoneyServiceImpl : BoneyService.BoneyServiceBase
     {
+        private Paxos _paxos;
+        public BoneyServiceImpl(Paxos paxos)
+        {
+            _paxos = paxos;
+        }
+        public override Task<CompareSwapReply> CompareAndSwap(CompareSwapRequest request, ServerCallContext context)
+        {
+            CompareSwapReply reply = new();
 
+            // how to know when frozen?
+
+            reply.Outvalue = _paxos.Consensus(request.Slot, request.Invalue);
+
+
+            return Task.FromResult(reply);
+        }
     }
 }
