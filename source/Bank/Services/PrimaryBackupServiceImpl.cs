8e291740a59cdea2a73241a914675788d6c194d6
ShadowGoblet20354 <62452709+ShadowGoblet20354@users.noreply.github.com>
2022-10-25 18:01:17 +0100

    Started implementing PrimaryBackupService

---
+++ source/Bank/Services/PrimaryBackupServiceImpl.cs
@@ -0,0 +1,39 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Services
+{
+    internal class PrimaryBackupServiceImpl : PrimaryBackupService.PrimaryBackupServiceBase
+    {
+
+
+
+
+        public override Task<PromiseReply> Promise(PromiseRequest request, ServerCallContext context)
+        {
+            PromiseReply reply = new();
+
+
+            return Task.FromResult(reply);
+        }
+
+        public override Task<CommitReply> Commit(CommitRequest request, ServerCallContext context)
+        {
+            CommitReply reply = new();
+
+
+            return Task.FromResult(reply);
+        }
+        public override Task<ListPendingReply> ListPending(ListPendingRequest request, ServerCallContext context)
+        {
+            ListPendingReply reply = new();
+
+
+            return Task.FromResult(reply);
+        }
+    }
+}
