43a9defd617fdb3c01cfdd188fabb00e0c9b31c4
diogoDSN <diogofgsnogueira@gmail.com>
2022-10-25 16:50:15 +0100

    Finished Test Grpc

--- DAD-Support-Material/TestGrpc/TestGrpc/Client.cs
+++ DAD-Support-Material/TestGrpc/TestGrpc/Client.cs
@@ -1,7 +1,61 @@
-﻿internal class Client
+﻿using Grpc.Core.Interceptors;
+using Grpc.Core;
+using Grpc.Net.Client;
+using static Grpc.Core.Interceptors.Interceptor;
+
+internal class Client
 {
-    private static void Main(string[] args)
+
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        const int ServerPort = 1001;
+        const string ServerHostname = "localhost";
+        AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+        var clientInterceptor = new ClientInterceptor();
+        GrpcChannel channel = GrpcChannel.ForAddress("http://" + ServerHostname + ":" + ServerPort);
+        CallInvoker interceptingInvoker = channel.Intercept(clientInterceptor);
+
+        var client = new TestService.TestServiceClient(interceptingInvoker);
+
+        Request request = new Request { MyInt = 5 };
+
+        try
+        {
+            Reply reply = client.Test(request);
+            Console.WriteLine("Reply received!");
+
+        } catch (RpcException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+
+        Console.ReadKey();
+    }
+}
+public class ClientInterceptor : Interceptor {
+
+    public override TResponse BlockingUnaryCall<TRequest, TResponse>(TRequest request, ClientInterceptorContext<TRequest, TResponse> context, BlockingUnaryCallContinuation<TRequest, TResponse> continuation)
+    {
+
+        bool cancel = false;
+
+        //ClientInterceptorContext<TRequest, TResponse> modifiedContext =
+        //    new ClientInterceptorContext<TRequest, TResponse>(context.Method, context.Host, context.Options.WithCancellationToken(new System.Threading.CancellationToken(cancel)));
+        if (cancel)
+        {
+            throw new RpcException(new Status(StatusCode.Unavailable, "I am frozen!"));
+        }
+        Console.Write("Intercepted outgoing call...\n");
+        TResponse response = base.BlockingUnaryCall(request, context, continuation);
+
+        //TResponse response = base.BlockingUnaryCall(request, modifiedContext, continuation);
+        Console.WriteLine("Intercepted incomming response...\n");
+        return response;
     }
 }
--- DAD-Support-Material/TestGrpc/TestsGrpcServer/Server.cs
+++ DAD-Support-Material/TestGrpc/TestsGrpcServer/Server.cs
@@ -1,7 +1,58 @@
-﻿internal class Server
+﻿using Grpc.Core.Interceptors;
+using Grpc.Core;
+
+
+class TestServer
 {
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        const int ServerPort = 1001;
+        const string ServerHostname = "localhost";
+
+        Server server = new Server
+        {
+            Services = { TestService.BindService(new TestServiceImpl()).Intercept(new ServerInterceptor()) },
+            Ports = { new ServerPort(ServerHostname, ServerPort, ServerCredentials.Insecure) }
+        };
+        server.Start();
+        Console.WriteLine("ChatServer server listening on port " + ServerPort);
+        Console.WriteLine("Press any key to stop the server...");
+        Console.ReadKey();
+
+        server.ShutdownAsync().Wait();
+
     }
+}
+internal class TestServiceImpl : TestService.TestServiceBase
+{
+    public TestServiceImpl()
+    {
+    }
+
+    public override Task<Reply> Test(Request request, ServerCallContext context)
+    {
+        Console.WriteLine("Tests request being served");
+        return Task.FromResult(new Reply());
+    }
+}
+
+public class ServerInterceptor : Interceptor
+{
+
+    public override Task<TResponse> UnaryServerHandler<TRequest, TResponse>(TRequest request, ServerCallContext context, UnaryServerMethod<TRequest, TResponse> continuation)
+    {
+        bool cancel = true;
+
+        Console.WriteLine("Intercepted incoming call");
+
+        if (cancel)
+        {
+            throw new RpcException(new Status(StatusCode.Unavailable, "Server is Frozen"));
+        }
+
+        Task<TResponse> reply = base.UnaryServerHandler(request, context, continuation);
+        Console.WriteLine("Intercepted outgoing reply");
+        return reply;
+    }
+
 }
